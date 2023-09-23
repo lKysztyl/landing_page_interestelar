dc5416676f4abb9f8a679e070ea63bbea5ee35bc
Patrick Casimiro <patrickcasimiroz@gmail.com>
2023-09-22 22:15:26 -0300

    feat: Adicionando uma barra de metacrit ao hero.
    Modificações:
    
    - Index/_Hero: Criação do brand-logo e da barra de metacrit.
    - Img: adição de mais duas imagens.
    - Tailwind.config: Adição de classes personalizadas e uma extenção do Grid.

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,42 @@ module.exports = {
       },
       spacing: {
         '137': '37rem',
+        'ul-sm': '1px', 
       },
       boxShadow: {
         'inner-smoke': 'inset 0 -10px 50px 70px rgb(0 0 0 / 0.05)',
+      },
+      gridTemplateRows: {
+        // Simple 8 row grid
+        'sm-row': '10% 45% 45%'
       }
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        '.translate-center': {
+          transform: 'translate(-50%, -50%)',
+          top: '50%',
+          left: '50%',
+          position: 'absolute',
+        },
+        '.flex-center': {
+          display: 'flex',
+          'justify-content': 'center',
+          'aling-items': 'center',
+        },
+        '.flex-between': {
+          display: 'flex',
+          'justify-content': 'space-between',
+          'aling-items': 'center',
+        },
+        '.backdrop-ul-sm': {
+          'backdrop-filter': 'blur(1px)'
+        },
+      };
+
+      addUtilities(newUtilities, ['responsive', 'hover']);
+    },
+  ],
 } 
